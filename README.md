3bcb35a72e09935246c527fd062e799e6022c539
Maury Miller <mmiller@g2llc.com>
2015-07-10 09:14:14 -0700

    Initial checkin for example application

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# sparkExample
+# Apache Spark
+
+Spark is a fast and general cluster computing system for Big Data. It provides
+high-level APIs in Scala, Java, and Python, and an optimized engine that
+supports general computation graphs for data analysis. It also supports a
+rich set of higher-level tools including Spark SQL for SQL and structured
+data processing, MLlib for machine learning, GraphX for graph processing,
+and Spark Streaming for stream processing.
+
+<http://spark.apache.org/>
+
+
+## Example
+The example can be run with this command:
+LOCATION OF YOUR SPARK APPLICATION/bin/spark-submit --class "org.mmiller.spark.SparkApplication" --master local[4]   target/SparkExample-1.0-SNAPSHOT.jar
---
+++ src/main/java/org/mmiller/spark/SparkApplication.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mmiller.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+
+/**
+ *
+ * @author Maury Miller
+ */
+public class SparkApplication {
+    public static void main(String[] args) {
+        String logFile = "textfile"; // Should be some file on your system
+        SparkConf conf = new SparkConf().setAppName("Spark Application");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(new Function<String, Boolean>() {
+            @Override
+            public Boolean call(String s) {
+                return s.contains("a");
+            }
+        }).count();
+
+        long numBs = logData.filter(new Function<String, Boolean>() {
+            @Override
+            public Boolean call(String s) {
+                return s.contains("b");
+            }
+        }).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+    }
+}
